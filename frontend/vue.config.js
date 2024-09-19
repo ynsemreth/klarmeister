67c31de14e61bf10697549cc65ef38da7bfa9dde
Yunus Emre <yunussemreth@gmail.com>
2024-09-19 03:45:49 +0300

    s

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,13 +2,5 @@ module.exports = {
   devServer: {
     host: '0.0.0.0',
     port: 5000,
-    allowedHosts: 'all', // Tüm ağlardan erişim izni verir
-    proxy: {
-      '/api': {
-        target: 'http://217.160.138.215:5000/',
-        changeOrigin: true,
-        pathRewrite: { '^/api': '' },
-      },
-    },
-  },
+  }
 };
