e29b3eaf4be3127db7cb37b7ba4e69712d7daf66
Yunus Emre <yunussemreth@gmail.com>
2024-09-20 06:45:34 +0300

    fix

--- frontend/src/homePage/homePage.vue
+++ frontend/src/homePage/homePage.vue
@@ -21,8 +21,8 @@
         <!-- Responsive ve Şekilli Video Kapsayıcısı -->
         <div class="custom-video-container">
             <div class="custom-video">
-                <video autoplay loop muted playsinline class="custom-video-player">
-                    <source src="@/assets/wrapper.mp4" type="video/mp4" />
+                <video autoplay controls muted playsinline class="custom-video-player">
+                    <source src="@/assets/video.mp4" type="video/mp4" />
                     Tarayıcınız video etiketini desteklemiyor.
                 </video>
             </div>
@@ -67,6 +67,15 @@
         </div>
     </el-col>
 
+    <el-row justify="center" class="reference-section">
+        <h2 class="reference-title">Zahlreiche Kunden Vertrauen auf Vierless</h2>
+        <el-carousel :interval="5000" arrow="always" autoplay>
+            <el-carousel-item v-for="(reference, index) in references" :key="index">
+                <img :src="reference.image" :alt="reference.name" class="reference-logo" />
+            </el-carousel-item>
+        </el-carousel>
+    </el-row>
+
     <el-col :span="24" class="new-section">
         <el-row :gutter="10">
             <el-col :span="14" class="left-section">
@@ -85,16 +94,22 @@
                         </h3>
                         <div class="case_tags-collection">
                             <div class="case_tag">
-                                <el-icon><Document /></el-icon>
+                                <el-icon>
+                                    <Document />
+                                </el-icon>
                                 <div class="case_tag-text">Design System</div>
                             </div>
-                            
+
                             <div class="case_tag">
-                                <el-icon><Refresh /></el-icon>
+                                <el-icon>
+                                    <Refresh />
+                                </el-icon>
                                 <div class="case_tag-text">Figma to Webflow</div>
                             </div>
                             <div class="case_tag">
-                                <el-icon><MagicStick /></el-icon>
+                                <el-icon>
+                                    <MagicStick />
+                                </el-icon>
                                 <div class="case_tag-text">Dynamic Lotties</div>
                             </div>
                         </div>
@@ -112,25 +127,28 @@
 
             <el-col :span="10" class="right-section">
                 <div class="big-case_right">
-                    <div class="big-case_right-video">
+                    <div class="big-case_right-content">
                         <video controls muted class="custom-card-player">
                             <source src="@/assets/sehlen_video.mp4" type="video/mp4" />
                             Tarayıcınız video etiketini desteklemiyor.
                         </video>
-                    </div>
 
-                    <div class="big-case_right-text">
-                        <h3 class="big-case_right-title">Gian-Marco Blum</h3>
-                        <div class="big-case_right-text-details">
-                            <span class="big-case_right-description is-first">Geschäftsführer</span>
-                            <span class="big-case_right-description">Candidate Flow GmbH</span>
+                        <div class="big-case_right-text">
+                            <h3 class="big-case_right-title">Sehlen Tourismus</h3>
+                            <div class="big-case_right-text-details">
+                                <span class="big-case_right-description">Dies ist das Einführungsvideo der Website</span>
+                            </div>
                         </div>
                     </div>
+                    <el-card class="box-card">
+                        <div class="image-section">
+                            <img src="@/assets/bilet.png" alt="Bilet Image" class="bilet-image" />
+                        </div>
+                    </el-card>
                 </div>
             </el-col>
         </el-row>
     </el-col>
-
 </el-row>
 </template>
 
@@ -140,10 +158,14 @@ import {
     onMounted,
     onUnmounted
 } from 'vue';
-import { Document, Refresh, MagicStick } from '@element-plus/icons-vue';
+import {
+    Document,
+    Refresh,
+    MagicStick
+} from '@element-plus/icons-vue';
 
 export default {
-    components:{
+    components: {
         Document,
         Refresh,
         MagicStick
@@ -151,17 +173,35 @@ export default {
     setup() {
         const isInView = ref(false);
 
+        const references = ref([{
+                name: 'Sehlen Tourismus',
+                image: '@/assets/sehlen_logo.png'
+            },
+            {
+                name: 'Allianz',
+                image: '@/assets/allianz.jpeg'
+            },
+            {
+                name: 'BMW',
+                image: '@/assets/bmw.jpeg'
+            },
+            {
+                name: 'Facebook',
+                image: '@/assets/face.png'
+            },
+            {
+                name: 'Rettig & Partner',
+                image: '@/assets/rettig.png'
+            },
+        ]);
+
         const handleScroll = () => {
             const videoSection = document.querySelector('.overlay-video-container');
             if (videoSection) {
                 const sectionPosition = videoSection.getBoundingClientRect().top;
                 const screenPosition = window.innerHeight / 5;
 
-                if (sectionPosition < screenPosition) {
-                    isInView.value = true;
-                } else {
-                    isInView.value = false;
-                }
+                isInView.value = sectionPosition < screenPosition;
             }
         };
 
@@ -175,6 +215,7 @@ export default {
 
         return {
             isInView,
+            references,
         };
     },
 };
@@ -442,7 +483,7 @@ export default {
     display: flex;
     justify-content: center;
     flex-direction: row;
-    gap: 20px; 
+    gap: 20px;
     margin-bottom: 20px;
     width: 70%;
 }
@@ -451,24 +492,24 @@ export default {
     display: flex;
     align-items: center;
     justify-content: center;
-    background-color: rgba(255, 255, 255, 0.1); 
+    background-color: rgba(255, 255, 255, 0.1);
     color: white;
     padding: 10px 20px;
     border-radius: 8px;
-    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2); 
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
     transition: background-color 0.3s ease, color 0.3s ease;
-    border: 1px solid rgba(255, 255, 255, 0.3); 
+    border: 1px solid rgba(255, 255, 255, 0.3);
     width: 80%;
 }
 
 .case_tag:hover {
-    background-color: rgba(255, 255, 255, 0.3); 
+    background-color: rgba(255, 255, 255, 0.3);
     color: white;
-    border-color: white; 
+    border-color: white;
 }
 
 .case_tag .el-icon {
-    margin-right: 8px; 
+    margin-right: 8px;
 }
 
 .case_tag-text {
@@ -477,44 +518,55 @@ export default {
     font-weight: 200;
     color: white;
 }
+
 .btn_wrapper {
-    background-color: rgba(255, 255, 255, 0.1); 
+    background-color: rgba(255, 255, 255, 0.1);
     color: white;
     padding: 10px 20px;
     border-radius: 8px;
-    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2); 
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
     transition: background-color 0.3s ease, color 0.3s ease;
-    border: 1px solid rgba(255, 255, 255, 0.3); 
+    border: 1px solid rgba(255, 255, 255, 0.3);
     font-weight: 600;
     text-transform: uppercase;
 }
 
 .btn_wrapper:hover {
-    background-color: rgba(255, 255, 255, 0.3); 
+    background-color: rgba(255, 255, 255, 0.3);
     color: white;
-    border-color: white; 
+    border-color: white;
 }
 
 .right-section .big-case_right {
+    font-family: 'Poppins', sans-serif;
     display: flex;
     flex-direction: column;
-    align-items: flex-end;
+    align-items: flex-start;
 }
 
-.big-case_right-video {
+.big-case_right-content {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    gap: 20px;
     width: 100%;
-    margin-bottom: 20px;
 }
 
 .custom-card-player {
-    width: 100%;
+    width: 50%;
     height: auto;
     border-radius: 10px;
 }
 
 .big-case_right-text {
-    text-align: right;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    width: 50%;
+    text-align: left;
     color: white;
+    line-height: 1.4;
 }
 
 .big-case_right-title {
@@ -522,49 +574,174 @@ export default {
     font-weight: 600;
     color: white;
     margin-bottom: 5px;
+    line-height: 1.6;
 }
 
 .big-case_right-description {
-    font-size: 16px;
+    font-size: 15px;
     font-weight: 400;
     color: white;
+    margin-bottom: 0;
+    display: inline;
+}
+
+.box-card {
+    display: flex;
+    margin-top: 10%;
+    width: 100%;
+    justify-content: center;
+    align-items: center;
+}
+
+.bilet-image {
+    max-width: 100%;
+    height: auto;
+    margin-top: 20px;
+    border-radius: 8px;
+}
+
+.reference-section {
+    padding: 50px 20px;
+    background-color: #000;
+    text-align: center;
+}
+
+.reference-title {
+    font-family: 'Montserrat', sans-serif;
+    font-size: 24px;
+    color: #00ff88;
+    margin-bottom: 20px;
+}
+
+.el-carousel {
+    width: 100%;
+}
+
+.reference-logo {
+    width: auto;
+    height: 50px;
+    max-width: 150px;
+    margin: 0 20px;
+}
+
+.el-carousel-item {
+    display: flex;
+    justify-content: center;
+    align-items: center;
 }
 
 @media (max-width: 768px) {
-    .hero-title {
-        font-size: 32px;
+
+    .custom-text-container {
+        display: none;
     }
 
-    .hero-subtitle {
-        font-size: 24px;
+    /* Diğer görseller ve videoları düzenlemek için */
+    .overlay-video-container {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
     }
 
+    .mockup-image {
+        width: 100%;
+        height: auto;
+    }
+
+    .custom-video-container {
+        position: absolute;
+        top: 23%;
+        left: 25%;
+        width: 45%;
+        height: 45%;
+        transform: perspective(800px) rotateX(-8deg) rotateY(8deg) skewY(-27deg);
+        border-radius: 10px;
+        overflow: hidden;
+        z-index: 2;
+        transition: transform 1s ease-in-out, top 1s ease-in-out, left 1s ease-in-out, width 1s ease-in-out, height 1s ease-in-out;
+    }
+
+    .custom-video-player {
+        width: 100%;
+        height: auto;
+    }
+
+    /* Information Section mobil ayarı */
     .information {
         flex-direction: column;
+        padding: 50px;
     }
 
-    .information-content,
-    .information-image {
+    .information-content {
         max-width: 100%;
         text-align: center;
+        padding: 10px;
     }
 
-    .information-text-title {
-        font-size: 20px;
+    .information-image {
+        max-width: 100%;
+        margin-top: 20px;
     }
 
-    .information-text-content {
-        font-size: 16px;
-    }
-
+    /* New Section mobil ayarı */
     .new-section {
         flex-direction: column;
+        padding: 20px;
     }
 
     .left-section,
     .right-section {
         width: 100%;
+        padding: 10px 0;
         text-align: center;
+    }
+
+    .case_tag {
+        padding: 10px 0;
+    }
+
+    .big-case_featured-image,
+    .custom-card-player,
+    .bilet-image {
+        width: 100%;
+        height: auto;
+    }
+
+    .big-case_right-content {
+        flex-direction: column;
+    }
+
+    .box-card {
+        justify-content: center;
+        align-items: center;
+        margin-top: 20px;
+    }
+
+    .reference-section {
+        padding: 30px 10px;
+    }
+
+    .reference-title {
+        font-size: 28px;
+    }
+
+    .reference-card {
+        padding: 5px;
+    }
+
+    .reference-image {
+        width: 60px;
+        height: 60px;
+        margin-bottom: 8px;
+    }
+
+    .reference-content h3 {
+        font-size: 16px;
+    }
+
+    .reference-content p {
+        font-size: 12px;
     }
 }
 </style>
