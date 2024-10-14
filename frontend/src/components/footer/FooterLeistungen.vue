c3fb3f896ace444bd1d2d94affb8c5dc121f771b
Yunus Emre <yunussemreth@gmail.com>
2024-10-14 11:22:18 +0300

    fix

--- frontend/src/components/footer/FooterLeistungen.vue
+++ frontend/src/components/footer/FooterLeistungen.vue
@@ -10,73 +10,189 @@
             <h2 class="gradient-title">1. Datenschutz auf einen Blick</h2>
             <h3 class="gradient-title">Allgemeine Hinweise</h3>
             <p>
-                Die folgenden Hinweise geben einen einfachen Überblick darüber,<br />
-                was mit Ihren personenbezogenen Daten passiert, wenn Sie diese<br />
-                Website besuchen. Personenbezogene Daten sind alle Daten, mit<br />
-                denen Sie persönlich identifiziert werden können. Ausführliche<br />
-                Informationen zum Thema Datenschutz entnehmen Sie unserer<br />
+                Die folgenden Hinweise geben einen einfachen Überblick darüber,
+                was mit Ihren personenbezogenen Daten passiert, wenn Sie diese
+                Website besuchen. Personenbezogene Daten sind alle Daten, mit
+                denen Sie persönlich identifiziert werden können. Ausführliche
+                Informationen zum Thema Datenschutz entnehmen Sie unserer
                 unter diesem Text aufgeführten Datenschutzerklärung.
             </p>
 
             <h3 class="gradient-title">Datenerfassung auf dieser Website</h3>
             <h4 class="gradient-title">Wer ist verantwortlich für die Datenerfassung auf dieser Website?</h4>
             <p>
-                Die Datenverarbeitung auf dieser Website erfolgt durch den<br />
-                Websitebetreiber. Dessen Kontaktdaten können Sie dem Abschnitt<br />
-                „Hinweis zur Verantwortlichen Stelle“ in dieser Datenschutzerklärung<br />
+                Die Datenverarbeitung auf dieser Website erfolgt durch den
+                Websitebetreiber. Dessen Kontaktdaten können Sie dem Abschnitt
+                „Hinweis zur Verantwortlichen Stelle“ in dieser Datenschutzerklärung
                 entnehmen.
             </p>
 
             <h4 class="gradient-title">Wie erfassen wir Ihre Daten?</h4>
             <p>
-                Ihre Daten werden zum einen dadurch erhoben, dass Sie uns diese<br />
-                mitteilen. Hierbei kann es sich z. B. um Daten handeln, die Sie in<br />
-                ein Kontaktformular eingeben. Andere Daten werden automatisch<br />
-                oder nach Ihrer Einwilligung beim Besuch der Website durch unsere<br />
-                IT-Systeme erfasst. Das sind vor allem technische Daten (z. B.<br />
-                Internetbrowser, Betriebssystem oder Uhrzeit des Seitenaufrufs).<br />
-                Die Erfassung dieser Daten erfolgt automatisch, sobald Sie diese<br />
+                Ihre Daten werden zum einen dadurch erhoben, dass Sie uns diese
+                mitteilen. Hierbei kann es sich z. B. um Daten handeln, die Sie in
+                ein Kontaktformular eingeben. Andere Daten werden automatisch
+                oder nach Ihrer Einwilligung beim Besuch der Website durch unsere
+                IT-Systeme erfasst. Das sind vor allem technische Daten (z. B.
+                Internetbrowser, Betriebssystem oder Uhrzeit des Seitenaufrufs).
+                Die Erfassung dieser Daten erfolgt automatisch, sobald Sie diese
                 Website betreten.
             </p>
 
             <h4 class="gradient-title">Wofür nutzen wir Ihre Daten?</h4>
             <p>
-                Ein Teil der Daten wird erhoben, um eine fehlerfreie Bereitstellung<br />
-                der Website zu gewährleisten. Andere Daten können zur Analyse<br />
+                Ein Teil der Daten wird erhoben, um eine fehlerfreie Bereitstellung
+                der Website zu gewährleisten. Andere Daten können zur Analyse
                 Ihres Nutzerverhaltens verwendet werden.
             </p>
 
             <h4 class="gradient-title">Welche Rechte haben Sie bezüglich Ihrer Daten?</h4>
             <p>
-                Sie haben jederzeit das Recht, unentgeltlich Auskunft über Herkunft,<br />
-                Empfänger und Zweck Ihrer gespeicherten personenbezogenen Daten<br />
-                zu erhalten. Sie haben außerdem ein Recht, die Berichtigung oder<br />
-                Löschung dieser Daten zu verlangen. Wenn Sie eine Einwilligung zur<br />
-                Datenverarbeitung erteilt haben, können Sie diese Einwilligung<br />
-                jederzeit für die Zukunft widerrufen. Außerdem haben Sie das Recht,<br />
-                unter bestimmten Umständen die Einschränkung der Verarbeitung<br />
-                Ihrer personenbezogenen Daten zu verlangen. Des Weiteren steht<br />
+                Sie haben jederzeit das Recht, unentgeltlich Auskunft über Herkunft,
+                Empfänger und Zweck Ihrer gespeicherten personenbezogenen Daten
+                zu erhalten. Sie haben außerdem ein Recht, die Berichtigung oder
+                Löschung dieser Daten zu verlangen. Wenn Sie eine Einwilligung zur
+                Datenverarbeitung erteilt haben, können Sie diese Einwilligung
+                jederzeit für die Zukunft widerrufen. Außerdem haben Sie das Recht,
+                unter bestimmten Umständen die Einschränkung der Verarbeitung
+                Ihrer personenbezogenen Daten zu verlangen. Des Weiteren steht
                 Ihnen ein Beschwerderecht bei der zuständigen Aufsichtsbehörde zu.
             </p>
 
             <h2 class="gradient-title">2. Hosting</h2>
             <h4 class="gradient-title">Externes Hosting</h4>
             <p>
-                Diese Website wird extern gehostet. Die personenbezogenen Daten,<br />
-                die auf dieser Website erfasst werden, werden auf den Servern des<br />
-                Hosters gespeichert. Hierbei kann es sich v. a. um IP-Adressen,<br />
-                Kontaktanfragen, Meta- und Kommunikationsdaten, Vertragsdaten,<br />
-                Kontaktdaten, Namen, Websitezugriffe und sonstige Daten, die über<br />
+                Diese Website wird extern gehostet. Die personenbezogenen Daten,
+                die auf dieser Website erfasst werden, werden auf den Servern des
+                Hosters gespeichert. Hierbei kann es sich v. a. um IP-Adressen,
+                Kontaktanfragen, Meta- und Kommunikationsdaten, Vertragsdaten,
+                Kontaktdaten, Namen, Websitezugriffe und sonstige Daten, die über
                 eine Website generiert werden, handeln.
             </p>
 
             <h2 class="gradient-title">3. Allgemeine Hinweise und Pflicht­informationen</h2>
             <h4 class="gradient-title">Datenschutz</h4>
             <p>
-                Die Betreiber dieser Seiten nehmen den Schutz Ihrer persönlichen<br />
-                Daten sehr ernst. Wir behandeln Ihre personenbezogenen Daten<br />
-                vertraulich und entsprechend den gesetzlichen Datenschutzvorschriften<br />
-                sowie dieser Datenschutzerklärung.
+                Die Betreiber dieser Seiten nehmen den Schutz Ihrer persönlichen
+                Daten sehr ernst. Wir behandeln Ihre personenbezogenen Daten
+                vertraulich und entsprechend den gesetzlichen Datenschutzvorschriften
+                sowie dieser Datenschutzerklärung. Wenn Sie diese Website benutzen,
+                werden verschiedene personenbezogene Daten erhoben. Personenbezogene
+                Daten sind Daten, mit denen Sie persönlich identifiziert werden können.
+                Die vorliegende Datenschutzerklärung erläutert, welche Daten wir
+                erheben und wofür wir sie nutzen. Sie erläutert auch, wie und zu
+                welchem Zweck das geschieht.
+            </p>
+
+            <p>
+                Wir weisen darauf hin, dass die Datenübertragung im Internet
+                (z. B. bei der Kommunikation per E-Mail) Sicherheitslücken
+                aufweisen kann. Ein lückenloser Schutz der Daten vor dem
+                Zugriff durch Dritte ist nicht möglich.
+            </p>
+
+            <h4 class="gradient-title">Hinweis zur verantwortlichen Stelle</h4>
+            <p>
+                Die verantwortliche Stelle für die Datenverarbeitung auf dieser
+                Website ist:
+            </p>
+
+            <p>
+                Abdullah Aydin <br />
+                Friedrichstraße 29 <br />
+                95706 Schirnding. <br />
+                E-Mail: info@Klarmeister.de <br />
+            </p>
+
+            <h4 class="gradient-title">Speicherdauer</h4>
+            <p>
+                Soweit innerhalb dieser Datenschutzerklärung keine speziellere
+                Speicherdauer genannt wurde, verbleiben Ihre personenbezogenen
+                Daten bei uns, bis der Zweck für die Datenverarbeitung entfällt.
+                Wenn Sie ein berechtigtes Löschersuchen geltend machen oder eine
+                Einwilligung zur Datenverarbeitung widerrufen, werden Ihre Daten
+                gelöscht, sofern wir keine anderen rechtlich zulässigen Gründe für
+                die Speicherung Ihrer personenbezogenen Daten haben (z. B. steuer-
+                oder handelsrechtliche Aufbewahrungsfristen); im letztgenannten
+                Fall erfolgt die Löschung nach Fortfall dieser Gründe.
+            </p>
+
+            <h4 class="gradient-title">Widerruf Ihrer Einwilligung zur Datenverarbeitung</h4>
+            <p>
+                Viele Datenverarbeitungsvorgänge sind nur mit Ihrer ausdrücklichen
+                Einwilligung möglich. Sie können eine bereits erteilte Einwilligung
+                jederzeit widerrufen. Die Rechtmäßigkeit der bis zum Widerruf
+                erfolgten Datenverarbeitung bleibt vom Widerruf unberührt.
+            </p>
+
+            <h4 class="gradient-title">Widerspruchsrecht gegen die Datenerhebung</h4>
+            <p>
+                WENN DIE DATENVERARBEITUNG AUF GRUNDLAGE VON ART. 6 ABS. 1 LIT. E ODER F DSGVO
+                ERFOLGT, HABEN SIE JEDERZEIT DAS RECHT, AUS GRÜNDEN, DIE SICH AUS IHRER BESONDEREN
+                SITUATION ERGEBEN, GEGEN DIE VERARBEITUNG IHRER PERSONENBEZOGENEN DATEN
+                WIDERSPRUCH EINZULEGEN; DIES GILT AUCH FÜR EIN AUF DIESE BESTIMMUNGEN GESTÜTZTES
+                PROFILING.
+            </p>
+
+            <h2 class="gradient-title">4. Datenerfassung auf dieser Website</h2>
+            <h4 class="gradient-title">Cookies</h4>
+            <p>
+                Unsere Internetseiten verwenden so genannte „Cookies“. Cookies sind
+                kleine Datenpakete und richten auf Ihrem Endgerät keinen Schaden an.
+                Sie werden entweder vorübergehend für die Dauer einer Sitzung (Session-Cookies)
+                oder dauerhaft (permanente Cookies) auf Ihrem Endgerät gespeichert.
+                Session-Cookies werden nach Ende Ihres Besuchs automatisch gelöscht.
+                Permanente Cookies bleiben auf Ihrem Endgerät gespeichert, bis Sie
+                diese selbst löschen oder eine automatische Löschung durch Ihren
+                Webbrowser erfolgt. Cookies können von uns (First-Party-Cookies)
+                oder von Drittunternehmen stammen (sog. Third-Party-Cookies).
+                Third-Party-Cookies ermöglichen die Einbindung bestimmter
+                Dienstleistungen von Drittunternehmen innerhalb von Webseiten
+                (z. B. Cookies zur Abwicklung von Zahlungsdienstleistungen).
+            </p>
+
+            <h4 class="gradient-title">Welche Cookies verwenden wir?</h4>
+            <p>
+                Cookies haben verschiedene Funktionen. Zahlreiche Cookies sind technisch
+                notwendig, da bestimmte Webseitenfunktionen ohne diese nicht funktionieren
+                würden (z. B. die Warenkorbfunktion oder die Anzeige von Videos). Andere
+                Cookies können zur Auswertung des Nutzerverhaltens oder zu Werbezwecken
+                verwendet werden.
+            </p>
+
+            <h2 class="gradient-title">5. Newsletter</h2>
+            <h4 class="gradient-title">Newsletterdaten</h4>
+            <p>
+                Wenn Sie den auf der Website angebotenen Newsletter beziehen möchten,
+                benötigen wir von Ihnen eine E-Mail-Adresse sowie Informationen, welche
+                uns die Überprüfung gestatten, dass Sie der Inhaber der angegebenen
+                E-Mail-Adresse sind und mit dem Empfang des Newsletters einverstanden
+                sind. Weitere Daten werden nicht bzw. nur auf freiwilliger Basis erhoben.
+                Diese Daten verwenden wir ausschließlich für den Versand der
+                angeforderten Informationen und geben diese nicht an Dritte weiter.
+            </p>
+
+            <h2 class="gradient-title">6. Plugins und Tools</h2>
+            <h4 class="gradient-title">YouTube mit erweitertem Datenschutz</h4>
+            <p>
+                Diese Website bindet Videos der Website YouTube ein. Betreiber der
+                Website ist die Google Ireland Limited („Google”), Gordon House, Barrow
+                Street, Dublin 4, Irland. Wenn Sie eine dieser Website besuchen, auf denen
+                YouTube eingebunden ist, wird eine Verbindung zu den Servern von YouTube
+                hergestellt. Dabei wird dem YouTube-Server mitgeteilt, welche unserer Seiten
+                Sie besucht haben. Wenn Sie in Ihrem YouTube-Account eingeloggt sind,
+                ermöglichen Sie YouTube, Ihr Surfverhalten direkt Ihrem persönlichen Profil
+                zuzuordnen. Dies können Sie verhindern, indem Sie sich aus Ihrem YouTube-Account ausloggen.
+            </p>
+
+            <h4 class="gradient-title">SSL- bzw. TLS-Verschlüsselung</h4>
+            <p>
+                Diese Seite nutzt aus Sicherheitsgründen und zum Schutz der Übertragung
+                vertraulicher Inhalte, wie zum Beispiel Bestellungen oder Anfragen, die Sie an
+                uns als Seitenbetreiber senden, eine SSL- bzw. TLS-Verschlüsselung. Eine
+                verschlüsselte Verbindung erkennen Sie daran, dass die Adresszeile des
+                Browsers von „http://“ auf „https://“ wechselt und an dem Schloss-Symbol
+                in Ihrer Browserzeile.
             </p>
         </div>
     </div>
@@ -100,6 +216,9 @@ export default {
 <style scoped>
 .container {
     max-width: 1200px;
+    /* Genişlik değerini daha da artır */
+    width: 100%;
+    /* Tam genişlikte olmasını sağla */
     height: auto;
     margin: 20px auto;
     padding: 0 15px;
@@ -108,37 +227,47 @@ export default {
     align-items: center;
 }
 
-.daten-h3-title{
-    font-family: 'Poppins', sans-serif;
-    font-size: 14px;
-    color: black;
-    text-align: center;
-}
-
-.daten-h1-subtitle{
-    font-family: 'Montserrat', sans-serif;
-    font-size: 36px;
-    font-weight: bold;
-    color: white;
-    text-align: center;
-    margin-bottom: 60px;
-}
-
 .card {
     background: linear-gradient(to bottom, #212121, #111111);
     border-radius: 16px;
     border: 5px solid rgba(255, 255, 255, 0.1);
     color: white;
     padding: 20px;
-    height: auto;
+    width: 100%;
+    max-width: 1000px;
     font-family: 'Poppins', sans-serif;
+    box-sizing: border-box;
+    height: auto;
+    /* Yüksekliği otomatik olarak ayarla */
+    overflow: visible;
+    /* Taşma olursa, içeriğin görünmesini sağla */
 }
 
 .card p {
     margin: 10px 0;
-    line-height: 1.6;
+    line-height: 1.8;
     color: white;
+    font-size: 16px;
     font-weight: 400;
+    width: 100%;
+    word-wrap: break-word;
+    /* Satırları otomatik olarak kaydır */
+}
+
+.daten-h3-title {
+    font-family: 'Poppins', sans-serif;
+    font-size: 14px;
+    color: black;
+    text-align: center;
+}
+
+.daten-h1-subtitle {
+    font-family: 'Montserrat', sans-serif;
+    font-size: 36px;
+    font-weight: bold;
+    color: white;
+    text-align: center;
+    margin-bottom: 60px;
 }
 
 .gradient-title {
@@ -149,7 +278,6 @@ export default {
     margin-bottom: 15px;
     border-bottom: 2px solid #4caf50;
 }
-
 
 @media (max-width: 768px) {
     .container {
